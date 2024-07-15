b408256fc92e7cf768f70ec3faa64ae9d339f42f
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-15 13:55:25 -0600

    feat: user model and bcrypt

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,20 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email)}
-  end 
-  
+    it { should validate_presence_of(:password_digest)}
+    it { should have_secure_password }
+  end
+
+  it 'can create a user with password' do
+    params = {
+      name: "Noah",
+      email: "noah@testing.com",
+      password: "12345678",
+      password_confirmation: "12345678"
+    }
+    noah = User.create(params)
+
+    expect(noah).to_not have_attribute(:password)
+    expect(noah.password_digest).to_not eq('12345678')
+  end
 end
