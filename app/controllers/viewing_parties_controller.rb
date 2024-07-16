fd323572e11fe6a48f71e237819d46798a0b437f
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-16 17:46:20 -0600

    feat: a user now must be logged in to create a viewing party

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,18 +1,27 @@
-class ViewingPartiesController < ApplicationController 
+class ViewingPartiesController < ApplicationController
+  before_action :require_user, only: :new
+
   def new
     @user = User.find(params[:user_id])
     @movie = Movie.find(params[:movie_id])
-  end 
-  
-  def create 
+  end
+
+  def create
     user = User.find(params[:user_id])
     user.viewing_parties.create(viewing_party_params)
     redirect_to "/users/#{params[:user_id]}"
-  end 
+  end
 
-  private 
+  private
 
-  def viewing_party_params 
+  def viewing_party_params
     params.permit(:movie_id, :duration, :date, :time)
-  end 
-end 
+  end
+
+  def require_user
+    if !current_user
+      flash[:error] = "you must log in"
+      redirect_to "/users/#{params[:user_id]}/movies/#{params[:movie_id]}/"
+    end
+  end
+end
