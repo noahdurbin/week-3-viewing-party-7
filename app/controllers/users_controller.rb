753bd3493dd7ebc10a46cffc81b091220ce0f790
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-16 16:26:17 -0600

    feat: user story 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,44 @@
-class UsersController < ApplicationController 
-  def new 
+class UsersController < ApplicationController
+  def new
     @user = User.new
-  end 
+  end
 
-  def show 
+  def show
     @user = User.find(params[:id])
-  end 
+  end
 
-  def create 
+  def create
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}"
       redirect_to user_path(user)
-    else  
+    else
       flash[:error] = user.errors.full_messages.to_sentence
       redirect_to register_path
-    end 
-  end 
+    end
+  end
 
   def login_form
 
-  end 
+  end
 
   def login_user
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      cookies[:location] = params[:location]
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}"
       redirect_to "/users/#{user.id}"
-    else 
+    else
       flash[:error] = "Bad Credentials, try again."
-      redirect_to "/login" 
-    end 
-  end 
+      redirect_to "/login"
+    end
+  end
 
-  private 
+  private
 
-  def user_params 
+  def user_params
     params.require(:user).permit(:name, :email, :password)
-  end 
-end 
+  end
+end
